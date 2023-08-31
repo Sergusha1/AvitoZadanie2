fc13e0ce50af51ddf6dfd4d2043bcd55cad8e5bc
Sergusha1 <143121390+Sergusha1@users.noreply.github.com>
2023-08-31 17:09:55 +0300

    Update avito.py

--- avito.py
+++ avito.py
@@ -7,7 +7,9 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 # Создание опций для режима инкогнито чтобы не выходить из аккаунта, если авторизация в браузере уже была
 chrome_options = webdriver.ChromeOptions()
-driver.get("https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363")
+chrome_options.add_argument("--incognito")
+driver = webdriver.Chrome(options=chrome_options)
+driver.switch_to.new_window('https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363')
 favorite_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Добавить в избранное')]")
 favorite_button.click()
 driver.quit()
